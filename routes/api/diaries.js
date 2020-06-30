105b094d587064f7745725ff17d4f519ffea6be8
KamMan <kam123ex@gmail.com>
2020-06-30 15:27:54 +0800

    build new verison

--- routes/api/diaries.js
+++ routes/api/diaries.js
@@ -8,26 +8,11 @@ const Diary = require("../../mondel/Diary");
 
 router.get("/", (req, res) => {
   Diary.find()
-    .select("title content date _id")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        diaries: docs.map((doc) => {
-          return {
-            title: doc.title,
-            content: doc.content,
-            date: doc.date,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url:
-                "https://backend-304cem.herokuapp.com/api/diaries/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
+    .then((result) => {
+      return res.status(201).json({
+        diaries: result,
+        success: true,
+      });
     })
     .catch((err) => {
       console.log(err);
